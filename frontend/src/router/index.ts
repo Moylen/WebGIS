204780066e9d80b05166f773e6344077e1bc68a6
Moylen <moylenrh@gmail.com>
2024-10-28 16:12:52 +0700

    Frontend: add route protect

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,29 +1,39 @@
-import HomePage from "../pages/HomePage.vue";
+import HomePage from '../pages/HomePage.vue';
 import RegisterPage from '../pages/RegisterPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '../pages/LoginPage.vue';
+import { useAuthStore } from '../store/auth.ts';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
     component: HomePage,
   },
   {
     path: '/register',
-    name: 'Register',
     component: RegisterPage,
   },
   {
     path: '/login',
-    name: 'Login',
     component: LoginPage,
-  }
-]
+    meta: {
+      requiresAuth: true,
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
+
+router.beforeEach((to, _, next) => {
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next({ path: '/login' });
+  } else {
+    next();
+  }
+});
 
 export default router;
